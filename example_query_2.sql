9c19510dc3167c72007f347bb24c2e842bbcaf71
Allen Day <allenday@allenday.com>
2022-10-12 19:47:20 +0800

    filter to inter-exchange

--- example_query_2.sql
+++ example_query_2.sql
@@ -1,7 +1,9 @@
 SELECT 
-  CASE addr_hot.address WHEN tx.from_address THEN addr_hot.exchange ELSE addr_usr.exchange END AS exchange,
-  CASE addr_hot.address WHEN tx.from_address THEN 'out' ELSE 'in' END AS io,
-  tx.*
+  tx.block_timestamp,
+  tx.`hash`,
+  tx.value / POW(10,18) AS value,
+  CASE addr_hot.address WHEN tx.from_address THEN addr_hot.exchange ELSE addr_usr.exchange END AS source,
+  CASE addr_hot.address WHEN tx.to_address THEN addr_hot.exchange ELSE addr_usr.exchange END AS target
 FROM
   `bigquery-public-data.crypto_ethereum.transactions` AS tx,
   `exchange-flow-demo.exchange_wallet_addresses.exchange_hotwallet_addresses` AS addr_hot,
@@ -10,3 +12,6 @@ WHERE TRUE
   AND block_timestamp >= '2019-10-23 00:00:00' 
   AND block_timestamp < '2019-10-24 00:00:00' 
   AND (tx.from_address = addr_hot.address OR tx.to_address = addr_usr.address)
+  AND value != 0
+GROUP BY source, target, block_timestamp, `hash`, value
+HAVING source != target
